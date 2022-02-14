ebe4691beeb1e23ae9a2bdff16b3fc83e7512b12
Vanilla <vetal-vetal-vv@mail.ru>
2022-02-14 11:35:50 +0300

    test Polt

--- app/src/main/java/com/example/poidem_gulyat/ui/splashFragment/SplashActivity.kt
+++ app/src/main/java/com/example/poidem_gulyat/ui/splashFragment/SplashActivity.kt
@@ -96,6 +96,10 @@ class SplashActivity : AppCompatActivity(), CoroutineScope {
         //
         //
         //
+
+        //
+        //
+        //
         launch {
             viewModelSplash.prefStateFlow.collect {
                 when (it) {
